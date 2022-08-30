26fbfe6caf039832661f33d8e60606608daa1489
“Arvinder” <Arvinder.Bhurji@ril.com>
2022-08-30 21:50:22 +0530

    initial  commit

---
+++ UI_Navigator/BlueViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BlueViewController.swift
+//  uinavcontrol
+//
+//  Created by Arvinder Bhurji on 30/08/22.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Hey! The Blue screen is displayed"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UI_Navigator/RedViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RedViewController.swift
+//  uinavcontrol
+//
+//  Created by Arvinder Bhurji on 30/08/22.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "We are navigated to the Red screen"
+        
+        print("Click on the arrow key to go back!")
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
